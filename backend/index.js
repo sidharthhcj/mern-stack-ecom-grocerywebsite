cef10c3dad963af8aae488858a51aa68005159da
sidharthhcj <sidharthss3337544@gmail.com>
2026-03-06 12:12:02 +0530

    correct error

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 await connectCloudinary();
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173",""];
 //middlewares
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
